37ef2c474a65cbdb7be1c352953cd4bf895bbffa
Jairo Santiago <jairosxd1992@gmail.com>
2021-08-23 14:49:49 +0200

    updated ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,27 +2,43 @@ provider "aws" {
   region = "us-west-2"
 }
 
+variable "key_id" {
+  description = "ssl key pair used to access the bastion instance via SSH"
+  type        = string
+  sensitive   = true
+}
+
 resource "aws_security_group" "webservers" {
   name        = "allow_http"
   description = "Allow http inbound traffic"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "webserver" {
-    ami = "ami-07ba77e2ad0ff38b8"
+    ami = "ami-083ac7c7ecf9bb9b0"
     instance_type = "t2.micro"
     vpc_security_group_ids = ["${aws_security_group.webservers.id}"]
+    associate_public_ip_address=true
+    key_name = var.key_id 
 }
